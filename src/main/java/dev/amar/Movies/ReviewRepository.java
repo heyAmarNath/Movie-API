a7d6027c2ee397e2c34b1e8db7d44d8e2a46bef5
Amar Nath <nthvpsn@gmail.com>
2023-07-08 20:51:26 +0530

    first commit

---
+++ src/main/java/dev/amar/Movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.amar.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
